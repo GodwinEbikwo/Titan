c79aa4c6fe392e7a14ef1b5ff86f69e421c300d8
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2020-12-13 12:09:38 +0000

    ok

--- pages/test.js
+++ pages/test.js
@@ -5,12 +5,6 @@ export default function test() {
   return (
     <div>
       <h1>Test screen</h1>
-      <Image
-        src="https://res.cloudinary.com/dqv9mfbvt/image/upload/v1596882037/ct-page-svg_hbpbbu.svg"
-        width={950}
-        height={950}
-        alt="image"
-      />
     </div>
   );
 }
